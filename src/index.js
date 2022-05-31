7ef84b829067215eec8da6170b3f502479700d71
Saad Niazi <saadniazifed@gmail.com>
2022-05-31 13:40:06 +0500

    Adding the cities dynamically.
    The cities are being added now dynamically in the console.log based on the user input.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+const getLocationBtn = document.querySelector(".getLocationBtn");
+
 async function getWeatherData() {
   //Use the try and catch method inside the async function.
+
   try {
+    const getLocation = document.querySelector("#location").value;
+
     //Fetch the weather of London from the API. Use await.
     const response = await fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=Islamabad&APPID=c37741fb2b2886b1cd8210bf805f8c57",
+      `http://api.openweathermap.org/data/2.5/weather?q=${getLocation}&APPID=c37741fb2b2886b1cd8210bf805f8c57`,
       { mode: "cors" }
     );
+
     //Pass the json data using JSON method in a variable.
     const data = await response.json();
+
+    //Creating an object which is giving me the values related to Weather
     const dataObj = {
       name: data.name,
       description: data.weather[0].description,
@@ -16,10 +24,11 @@ async function getWeatherData() {
       humidity: data.main.humidity,
     };
     console.log(dataObj);
+
     //If there's an error, console log it.
   } catch (err) {
     console.log(err);
   }
 }
 
-getWeatherData();
+getLocationBtn.addEventListener("click", getWeatherData);
