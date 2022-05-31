13b83c540a08a0c860c7ef3b64910c19ba205c74
Saad Niazi <saadniazifed@gmail.com>
2022-05-31 15:46:33 +0500

    Converted Temperature
    Temperature has been converted from Kelvin to Celsius.

--- src/index.js
+++ src/index.js
@@ -8,27 +8,30 @@ async function getWeatherData() {
 
     //Fetch the weather of London from the API. Use await.
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${getLocation}&APPID=c37741fb2b2886b1cd8210bf805f8c57`,
+      `http://api.openweathermap.org/data/2.5/weather?q=${getLocation}&units=metric&APPID=c37741fb2b2886b1cd8210bf805f8c57`,
       { mode: "cors" }
     );
 
     //Pass the json data using JSON method in a variable.
     const data = await response.json();
-
-    //Creating an object which is giving me the values related to Weather
-    const dataObj = {
-      name: data.name,
-      description: data.weather[0].description,
-      temp: data.main.temp,
-      feelsLike: data.main.feels_like,
-      humidity: data.main.humidity,
-    };
-    console.log(dataObj);
+    dataObject(data);
 
     //If there's an error, console log it.
   } catch (err) {
     console.log(err);
   }
 }
+
+const dataObject = (data) => {
+  const dataObj = {
+    name: data.name,
+    country: data.sys.country,
+    description: data.weather[0].description,
+    temp: data.main.temp,
+    feelsLike: data.main.feels_like,
+    humidity: data.main.humidity,
+  };
+  console.log(dataObj);
+};
 
 getLocationBtn.addEventListener("click", getWeatherData);
